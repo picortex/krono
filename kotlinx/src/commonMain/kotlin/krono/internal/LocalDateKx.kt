5096e0a19331b5c7d2ab5dcc2dcb2defcdc8e52c
andylamax <andylamax@programmer.net>
2022-11-29 00:59:10 +0300

    Fixed LocalDateKx

--- kotlinx/src/commonMain/kotlin/krono/internal/LocalDateKx.kt
+++ kotlinx/src/commonMain/kotlin/krono/internal/LocalDateKx.kt
@@ -3,9 +3,10 @@ package krono.internal
 import krono.DayOfWeek
 import krono.LocalDate
 import krono.Month
+import krono.utils.DaysOfMonth
 import kotlinx.datetime.LocalDate as KxLocalDateTime
 
-internal class LocalDateKx(private val wrapped: KxLocalDateTime) : AbstractDateLike() {
+internal class LocalDateKx(private val wrapped: KxLocalDateTime) : AbstractDateLike<LocalDate>(), LocalDate {
     override val year: Int get() = wrapped.year
     override val monthNumber: Int get() = wrapped.monthNumber
     override val month: Month get() = Month.valueOf(wrapped.month.name)
@@ -13,15 +14,9 @@ internal class LocalDateKx(private val wrapped: KxLocalDateTime) : AbstractDateL
     override val dayOfWeek: DayOfWeek get() = DayOfWeek.valueOf(wrapped.dayOfWeek.name)
     override val dayOfYear: Int get() = wrapped.dayOfYear
 
-//    override fun compareTo(other: LocalDate): Int = wrapped.compareTo(KxLocalDateTime(other.year, other.monthNumber, other.dayOfMonth))
+    override fun compareTo(other: LocalDate): Int = wrapped.compareTo(KxLocalDateTime(other.year, other.monthNumber, other.dayOfMonth))
 
-    override fun atDate(date: Int): LocalDate = LocalDateKx(KxLocalDateTime(year, monthNumber, date))
+    override fun atDate(date: Int) = LocalDateKx(KxLocalDateTime(year, monthNumber, date))
 
-    override fun isBefore(other: LocalDate): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun isAfter(other: LocalDate): Boolean {
-        TODO("Not yet implemented")
-    }
+    override fun atEndOfMonth() = atDate(DaysOfMonth(year, monthNumber))
 }
