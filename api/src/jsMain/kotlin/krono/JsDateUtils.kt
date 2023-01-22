3d5ce10bb36d8e4f012a4d97f488b61dd4c25772
andylamax <andylamax@programmer.net>
2023-01-22 13:08:11 +0300

    Added LocalDate and JsDate interoperabel functions

---
+++ api/src/jsMain/kotlin/krono/JsDateUtils.kt
@@ -0,0 +1,14 @@
+@file:JsExport
+@file:Suppress("NON_EXPORTABLE_TYPE")
+
+package krono
+
+import kotlin.js.Date
+
+fun dateOf(ld: LocalDate): Date = Date(ld.year, ld.monthNumber - 1, ld.dayOfMonth)
+
+fun dateOrNullOf(ld: LocalDate?): Date? = if (ld == null) null else dateOf(ld)
+
+fun localDateOf(d: Date) = LocalDate(d.toISOString())
+
+fun localOrNullOf(d: Date?) = LocalDateOrNull(d?.toISOString())
