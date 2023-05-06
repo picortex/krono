4426123416408a2b5057c989ee35a3c90e717217
andylamax <andylamax@programmer.net>
2023-05-06 23:34:06 +0300

    Added expect toBeArround and expect.toBe with tolerance

---
+++ kotlinx/src/commonMain/kotlin/krono/KxInstatUtils.kt
@@ -0,0 +1,8 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package krono
+
+import krono.internal.InstantImpl
+import kotlinx.datetime.Instant as KxInstant
+
+inline fun KxInstant.toInstant(): Instant = InstantImpl(toEpochMilliseconds())
