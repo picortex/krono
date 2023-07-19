62332aea8f33d53d4510a00fb546b99b8688f010
andylamax <andylamax@programmer.net>
2023-07-19 18:06:04 +0300

    Fixed the date format bug

--- api/src/commonMain/kotlin/krono/DateTimePresenter.kt
+++ api/src/commonMain/kotlin/krono/DateTimePresenter.kt
@@ -11,7 +11,7 @@ class DateTimePresenter(
     val pattern: PresenterPattern
 ) {
     val formatter = pattern.formatter
-    fun toDateString(): String = formatter.date.formatDate(date.year, date.dayOfMonth, date.dayOfMonth)
+    fun toDateString(): String = formatter.date.formatDate(date.year, date.monthNumber, date.dayOfMonth)
     fun toTimeString(): String = formatter.time.formatTime(time.hour, time.minute, time.second)
     fun toDateTimeString(): String = formatter.dateTime.formatDateTime(date.year, date.monthNumber, date.dayOfMonth, time.hour, time.minute, time.second)
 }
